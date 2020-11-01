c56c2271cd40154560d7f92d28c3a701ebe83a16
tobiichiamane <tobiichiamane@outlook.com>
2020-11-01 15:56:35 +0800

    完成基础功能

--- winver.cpp
+++ winver.cpp
@@ -2,7 +2,24 @@
 #include "RegistryHelper.h"
 #include "RegistryError.h"
 
+#define CURRENT_VERSION_SUBKEY L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"
+
+#define RegString(value) \
+	std::wstring value = ::RegGetString(HKEY_LOCAL_MACHINE, CURRENT_VERSION_SUBKEY, L#value)
+
+#define RegDword(value) \
+	DWORD value = ::RegGetDword(HKEY_LOCAL_MACHINE, CURRENT_VERSION_SUBKEY, L#value)
+
 int main(int argc, char* argv[])
 {
-	
+	RegString(ProductName);
+	RegString(DisplayVersion);
+	RegString(CurrentBuildNumber);
+	RegDword(UBR);
+
+	std::wcout
+		<< ProductName
+		<< " " << DisplayVersion
+		<< " (Build " << CurrentBuildNumber << "." << UBR << ")"
+		<< std::endl;
 }
